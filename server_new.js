4a46731472b7ecf0df0157448d9a6b24e09d0754
changbingding <changbing.ding@kaaxaa.com>
2014-06-11 22:21:22 +0800

    update agent server

--- server_new.js
+++ server_new.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var urlUtil = require('url');
+var qs = require("querystring");
 
 var request = require("request");
 
@@ -50,12 +51,20 @@ server.use(function(req, res, next){
 
 server.use(function(req, res, next){
     var url = req.url;
-    var reg = /login\.html$/;
+    var reg = /(login|userLogin)(\.html|\.aspx)?$/i;
     var urlInfo = urlUtil.parse(url);
     var loginUrl = config.loginUrl;
     var verifyUrl = config.verifyUrl;
     if(loginUrl && reg.test(urlInfo.pathname)){
-        request(loginUrl).pipe(res);
+        if(req.method == "POST"){
+            var data = req.body;
+            var dataStr =   qs.stringify(data);
+            var options = {url:loginUrl,headers:req.headers,encoding:null,followRedirect:false,body:dataStr,method:"POST"};
+            request(options).pipe(res);
+        }else{
+            request(loginUrl).pipe(res);
+        }
+
     } else{
         if(verifyUrl){
             request(verifyUrl, function(err, vRes, body){
